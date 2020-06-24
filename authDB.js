0e6781cd3877e732299c4792b804722756589fd5
DanielOsorio24 <danielosorio761@gmail.com>
2020-06-24 00:39:05 -0500

    1 file modified

--- authDB.js
+++ authDB.js
@@ -5,6 +5,13 @@ const seed = {
     surName: 'Barba',
     id: '5-706-1803',
     state: 8,
+  },
+   'danielosorio761@gmail.com': {
+    password: '12345678',
+    firstName: 'Daniel',
+    surName: 'Osorio',
+    id: '8-888-888',
+    state: 8,
   }
 };
 
@@ -18,7 +25,11 @@ if(prevAuthDB){
 export default {
   db: authDB,
   add(data){
+    
     authDB[data.email] = data;
     localStorage.setItem("@authDB", JSON.stringify(authDB));
+  },
+  setUser(user){
+    localStorage.setItem("@authUser", user);
   }
 };
